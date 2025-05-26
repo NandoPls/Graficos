cea69e09db3be710c2f5310e670c7493249dbe5d
NandoPls <110955834+NandoPls@users.noreply.github.com>
2025-05-26 19:41:59 -0400

    Actualiza datos de mayo 2025

--- src/storeData.js
+++ src/storeData.js
@@ -6,7 +6,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 1164, boletas: 608, conversion: 52.2 },
       { month: 'Marzo', flujo: 1470, boletas: 802, conversion: 54.6 },
       { month: 'Abril', flujo: 1314, boletas: 705, conversion: 53.7 },
-      { month: 'Mayo', flujo: 756, boletas: 416, conversion: 55.0 }
+      { month: 'Mayo', flujo: 1081, boletas: 470, conversion: 43.5 }
     ]
   },
   {
@@ -16,7 +16,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 6091, boletas: 477, conversion: 7.8 },
       { month: 'Marzo', flujo: 7060, boletas: 531, conversion: 7.5 },
       { month: 'Abril', flujo: 6333, boletas: 547, conversion: 8.6 },
-      { month: 'Mayo', flujo: 4252, boletas: 293, conversion: 6.9 }
+      { month: 'Mayo', flujo: 6001, boletas: 418, conversion: 7.0 }
     ]
   },
   {
@@ -26,7 +26,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 5261, boletas: 659, conversion: 12.5 },
       { month: 'Marzo', flujo: 7014, boletas: 930, conversion: 13.3 },
       { month: 'Abril', flujo: 6308, boletas: 900, conversion: 14.3 },
-      { month: 'Mayo', flujo: 3931, boletas: 500, conversion: 12.7 }
+      { month: 'Mayo', flujo: 5499, boletas: 720, conversion: 13.1 }
     ]
   },
   {
@@ -36,7 +36,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 1596, boletas: 178, conversion: 11.2 },
       { month: 'Marzo', flujo: 2287, boletas: 293, conversion: 12.8 },
       { month: 'Abril', flujo: 1690, boletas: 238, conversion: 14.1 },
-      { month: 'Mayo', flujo: 772, boletas: 107, conversion: 13.9 }
+      { month: 'Mayo', flujo: 1078, boletas: 146, conversion: 13.5 }
     ]
   },
   {
@@ -46,7 +46,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 4061, boletas: 598, conversion: 14.7 },
       { month: 'Marzo', flujo: 4009, boletas: 581, conversion: 14.5 },
       { month: 'Abril', flujo: 3528, boletas: 539, conversion: 15.3 },
-      { month: 'Mayo', flujo: 1633, boletas: 292, conversion: 17.9 }
+      { month: 'Mayo', flujo: 2358, boletas: 431, conversion: 18.3 }
     ]
   },
   {
@@ -56,7 +56,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 8511, boletas: 384, conversion: 4.5 },
       { month: 'Marzo', flujo: 8775, boletas: 401, conversion: 4.6 },
       { month: 'Abril', flujo: 7562, boletas: 325, conversion: 4.3 },
-      { month: 'Mayo', flujo: 5194, boletas: 220, conversion: 4.2 }
+      { month: 'Mayo', flujo: 7111, boletas: 291, conversion: 4.1 }
     ]
   },
   {
@@ -66,7 +66,7 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 21524, boletas: 1051, conversion: 4.9 },
       { month: 'Marzo', flujo: 21810, boletas: 1142, conversion: 5.2 },
       { month: 'Abril', flujo: 18631, boletas: 1104, conversion: 5.9 },
-      { month: 'Mayo', flujo: 11663, boletas: 682, conversion: 5.8 }
+      { month: 'Mayo', flujo: 15508, boletas: 890, conversion: 5.7 }
     ]
   },
   {
@@ -76,48 +76,25 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 18345, boletas: 930, conversion: 5.1 },
       { month: 'Marzo', flujo: 19264, boletas: 889, conversion: 4.6 },
       { month: 'Abril', flujo: 15972, boletas: 911, conversion: 5.7 },
-      { month: 'Mayo', flujo: 10148, boletas: 514, conversion: 5.1 }
+      { month: 'Mayo', flujo: 13668, boletas: 671, conversion: 4.9 }
     ]
   },
   {
-    name: "Puerto montt",
+    name: "Puerto Montt",
     data: [
-      { month: 'Enero', flujo: 11472, boletas: 553, conversion: 4.8 },
-      { month: 'Febrero', flujo: 11332, boletas: 456, conversion: 4.0 },
-      { month: 'Marzo', flujo: 9579, boletas: 420, conversion: 4.4 },
-      { month: 'Abril', flujo: 6171, boletas: 242, conversion: 3.9 },
-      { month: 'Mayo', flujo: 3468, boletas: 176, conversion: 5.1 }
+      { month: 'Mayo', flujo: 4624, boletas: 219, conversion: 4.7 }
     ]
   },
   {
     name: "Parque Arauco",
     data: [
-      { month: 'Enero', flujo: 6751, boletas: 629, conversion: 9.3 },
-      { month: 'Febrero', flujo: 5571, boletas: 491, conversion: 8.8 },
-      { month: 'Marzo', flujo: 6237, boletas: 526, conversion: 8.4 },
-      { month: 'Abril', flujo: 5529, boletas: 566, conversion: 10.2 },
-      { month: 'Mayo', flujo: 3999, boletas: 324, conversion: 8.1 }
+      { month: 'Mayo', flujo: 5733, boletas: 470, conversion: 8.2 }
     ]
   },
   {
     name: "Tienda Arena MS",
     data: [
-      { month: 'Enero', flujo: 8003, boletas: 959, conversion: 12.0 },
-      { month: 'Febrero', flujo: 5586, boletas: 634, conversion: 11.3 },
-      { month: 'Marzo', flujo: 5561, boletas: 791, conversion: 14.2 },
-      { month: 'Abril', flujo: 4031, boletas: 549, conversion: 13.6 },
-      { month: 'Mayo', flujo: 2938, boletas: 478, conversion: 16.3 }
-    ]
-  },
-
-  {
-    name: "Resumen",
-    data: [
-      { month: 'Enero', flujo: 97245, boletas: 7896, conversion: 8.12 },
-      { month: 'Febrero', flujo: 89042, boletas: 6466, conversion: 7.26 },
-      { month: 'Marzo', flujo: 93066, boletas: 7306, conversion: 7.85 },
-      { month: 'Abril', flujo: 77069, boletas: 6626, conversion: 8.60 },
-      { month: 'Mayo', flujo: 48754, boletas: 4002, conversion: 8.21 }
+      { month: 'Mayo', flujo: 4205, boletas: 707, conversion: 16.8 }
     ]
   }
-]
+];
