2a0fbe43b39257fb6ca8d07b81bbfbcff3e51ba6
NandoPls <110955834+NandoPls@users.noreply.github.com>
2025-06-10 00:51:48 -0400

    Agrega datos de ventas por tienda hasta junio

--- src/storeData.js
+++ src/storeData.js
@@ -6,7 +6,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 1164, boletas: 608, conversion: 52.2 },
       { month: 'Marzo', flujo: 1470, boletas: 802, conversion: 54.6 },
       { month: 'Abril', flujo: 1314, boletas: 705, conversion: 53.7 },
-      { month: 'Mayo', flujo: 1360, boletas: 730, conversion: 53.7 }
+      { month: 'Mayo', flujo: 1360, boletas: 730, conversion: 53.7 },
+      { month: 'Junio', flujo: 357, boletas: 222, conversion: 62.2 }
     ]
   },
   {
@@ -16,7 +17,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 6091, boletas: 477, conversion: 7.8 },
       { month: 'Marzo', flujo: 7060, boletas: 531, conversion: 7.5 },
       { month: 'Abril', flujo: 6333, boletas: 547, conversion: 8.6 },
-      { month: 'Mayo', flujo: 7702, boletas: 529, conversion: 6.9 }
+      { month: 'Mayo', flujo: 7702, boletas: 529, conversion: 6.9 },
+      { month: 'Junio', flujo: 2788, boletas: 174, conversion: 6.2 }
     ]
   },
   {
@@ -26,7 +28,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 5261, boletas: 659, conversion: 12.5 },
       { month: 'Marzo', flujo: 7014, boletas: 930, conversion: 13.3 },
       { month: 'Abril', flujo: 6308, boletas: 900, conversion: 14.3 },
-      { month: 'Mayo', flujo: 6789, boletas: 864, conversion: 12.7 }
+      { month: 'Mayo', flujo: 6789, boletas: 864, conversion: 12.7 },
+      { month: 'Junio', flujo: 2312, boletas: 253, conversion: 10.9 }
     ]
   },
   {
@@ -36,7 +39,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 1596, boletas: 178, conversion: 11.2 },
       { month: 'Marzo', flujo: 2287, boletas: 293, conversion: 12.8 },
       { month: 'Abril', flujo: 1690, boletas: 238, conversion: 14.1 },
-      { month: 'Mayo', flujo: 1435, boletas: 190, conversion: 13.2 }
+      { month: 'Mayo', flujo: 1435, boletas: 190, conversion: 13.2 },
+      { month: 'Junio', flujo: 453, boletas: 62, conversion: 13.7 }
     ]
   },
   {
@@ -46,7 +50,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 4061, boletas: 598, conversion: 14.7 },
       { month: 'Marzo', flujo: 4009, boletas: 581, conversion: 14.5 },
       { month: 'Abril', flujo: 3528, boletas: 539, conversion: 15.3 },
-      { month: 'Mayo', flujo: 2984, boletas: 543, conversion: 18.2 }
+      { month: 'Mayo', flujo: 2984, boletas: 543, conversion: 18.2 },
+      { month: 'Junio', flujo: 713, boletas: 142, conversion: 19.9 }
     ]
   },
   {
@@ -56,7 +61,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 8511, boletas: 384, conversion: 4.5 },
       { month: 'Marzo', flujo: 8775, boletas: 401, conversion: 4.6 },
       { month: 'Abril', flujo: 7562, boletas: 325, conversion: 4.3 },
-      { month: 'Mayo', flujo: 8589, boletas: 370, conversion: 4.3 }
+      { month: 'Mayo', flujo: 8589, boletas: 370, conversion: 4.3 },
+      { month: 'Junio', flujo: 2502, boletas: 152, conversion: 6.1 }
     ]
   },
   {
@@ -66,7 +72,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 21524, boletas: 1051, conversion: 4.9 },
       { month: 'Marzo', flujo: 21810, boletas: 1142, conversion: 5.2 },
       { month: 'Abril', flujo: 18631, boletas: 1104, conversion: 5.9 },
-      { month: 'Mayo', flujo: 18093, boletas: 1044, conversion: 5.8 }
+      { month: 'Mayo', flujo: 18093, boletas: 1044, conversion: 5.8 },
+      { month: 'Junio', flujo: 4256, boletas: 286, conversion: 6.7 }
     ]
   },
   {
@@ -76,17 +83,19 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 18345, boletas: 930, conversion: 5.1 },
       { month: 'Marzo', flujo: 19264, boletas: 889, conversion: 4.6 },
       { month: 'Abril', flujo: 15972, boletas: 911, conversion: 5.7 },
-      { month: 'Mayo', flujo: 16708, boletas: 838, conversion: 5.0 }
+      { month: 'Mayo', flujo: 16708, boletas: 838, conversion: 5.0 },
+      { month: 'Junio', flujo: 4725, boletas: 265, conversion: 5.6 }
     ]
   },
-    {
+  {
     name: "Puerto Montt",
     data: [
       { month: 'Enero', flujo: 11472, boletas: 553, conversion: 4.8 },
       { month: 'Febrero', flujo: 11332, boletas: 456, conversion: 4.0 },
       { month: 'Marzo', flujo: 9579, boletas: 420, conversion: 4.4 },
       { month: 'Abril', flujo: 6171, boletas: 242, conversion: 3.9 },
-      { month: 'Mayo', flujo: 5495, boletas: 276, conversion: 5.0 }
+      { month: 'Mayo', flujo: 5495, boletas: 276, conversion: 5.0 },
+      { month: 'Junio', flujo: 1531, boletas: 83, conversion: 5.4 }
     ]
   },
   {
@@ -96,7 +105,8 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 5571, boletas: 491, conversion: 8.8 },
       { month: 'Marzo', flujo: 6237, boletas: 526, conversion: 8.4 },
       { month: 'Abril', flujo: 5529, boletas: 566, conversion: 10.2 },
-      { month: 'Mayo', flujo: 6970, boletas: 607, conversion: 8.7 }
+      { month: 'Mayo', flujo: 6970, boletas: 607, conversion: 8.7 },
+      { month: 'Junio', flujo: 2004, boletas: 208, conversion: 10.4 }
     ]
   },
   {
@@ -106,17 +116,19 @@ export const initialStoreData = [
       { month: 'Febrero', flujo: 5586, boletas: 634, conversion: 11.3 },
       { month: 'Marzo', flujo: 5561, boletas: 791, conversion: 14.2 },
       { month: 'Abril', flujo: 4031, boletas: 549, conversion: 13.6 },
-      { month: 'Mayo', flujo: 5015, boletas: 806, conversion: 16.1 }
+      { month: 'Mayo', flujo: 5015, boletas: 806, conversion: 16.1 },
+      { month: 'Junio', flujo: 1450, boletas: 232, conversion: 16.0 }
     ]
   },
-    {
+  {
     name: "Resumen",
     data: [
       { month: 'Enero', flujo: 97245, boletas: 7896, conversion: 8.12 },
       { month: 'Febrero', flujo: 89042, boletas: 6466, conversion: 7.26 },
       { month: 'Marzo', flujo: 93066, boletas: 7306, conversion: 7.85 },
       { month: 'Abril', flujo: 77069, boletas: 6626, conversion: 8.60 },
-      { month: 'Mayo', flujo: 81140, boletas: 6797, conversion: 8.38 }
+      { month: 'Mayo', flujo: 81140, boletas: 6797, conversion: 8.38 },
+      { month: 'Junio', flujo: 23091, boletas: 2079, conversion: 9.00 }
     ]
   }
 ];
