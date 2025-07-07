f56c96091d1bf0f88ffdf6771bc2ed6cf52b9624
NandoPls <Nandyyto2806@gmail.com>
2025-07-07 14:41:17 -0400

    Descripción de los cambiosss

--- src/storeData.js
+++ src/storeData.js
@@ -7,7 +7,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 1470, boletas: 802, conversion: 54.6 },
       { month: 'Abril', flujo: 1314, boletas: 705, conversion: 53.7 },
       { month: 'Mayo', flujo: 1360, boletas: 730, conversion: 53.7 },
-      { month: 'Junio', flujo: 1327, boletas: 703, conversion: 53.0 }
+      { month: 'Junio', flujo: 1425, boletas: 759, conversion: 53.3 },
+      { month: 'Julio', flujo: 266, boletas: 126, conversion: 47.4 }
     ]
   },
   {
@@ -18,7 +19,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 7060, boletas: 531, conversion: 7.5 },
       { month: 'Abril', flujo: 6333, boletas: 547, conversion: 8.6 },
       { month: 'Mayo', flujo: 7702, boletas: 529, conversion: 6.9 },
-      { month: 'Junio', flujo: 10763, boletas: 885, conversion: 8.2 }
+      { month: 'Junio', flujo: 11410, boletas: 943, conversion: 8.3 },
+      { month: 'Julio', flujo: 2498, boletas: 205, conversion: 8.2 }
     ]
   },
   {
@@ -29,7 +31,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 7014, boletas: 930, conversion: 13.3 },
       { month: 'Abril', flujo: 6308, boletas: 900, conversion: 14.3 },
       { month: 'Mayo', flujo: 6789, boletas: 864, conversion: 12.7 },
-      { month: 'Junio', flujo: 6211, boletas: 728, conversion: 11.7 }
+      { month: 'Junio', flujo: 6543, boletas: 770, conversion: 11.8 },
+      { month: 'Julio', flujo: 1514, boletas: 182, conversion: 12.0 }
     ]
   },
   {
@@ -40,7 +43,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 2287, boletas: 293, conversion: 12.8 },
       { month: 'Abril', flujo: 1690, boletas: 238, conversion: 14.1 },
       { month: 'Mayo', flujo: 1435, boletas: 190, conversion: 13.2 },
-      { month: 'Junio', flujo: 1416, boletas: 195, conversion: 13.8 }
+      { month: 'Junio', flujo: 1567, boletas: 211, conversion: 13.5 },
+      { month: 'Julio', flujo: 351, boletas: 28, conversion: 8.0 }
     ]
   },
   {
@@ -51,7 +55,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 4009, boletas: 581, conversion: 14.5 },
       { month: 'Abril', flujo: 3528, boletas: 539, conversion: 15.3 },
       { month: 'Mayo', flujo: 2984, boletas: 543, conversion: 18.2 },
-      { month: 'Junio', flujo: 2621, boletas: 491, conversion: 18.7 }
+      { month: 'Junio', flujo: 2865, boletas: 528, conversion: 18.4 },
+      { month: 'Julio', flujo: 561, boletas: 95, conversion: 16.9 }
     ]
   },
   {
@@ -62,7 +67,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 8775, boletas: 401, conversion: 4.6 },
       { month: 'Abril', flujo: 7562, boletas: 325, conversion: 4.3 },
       { month: 'Mayo', flujo: 8589, boletas: 370, conversion: 4.3 },
-      { month: 'Junio', flujo: 8308, boletas: 452, conversion: 5.4 }
+      { month: 'Junio', flujo: 8852, boletas: 476, conversion: 5.4 },
+      { month: 'Julio', flujo: 2030, boletas: 97, conversion: 4.8 }
     ]
   },
   {
@@ -73,7 +79,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 21810, boletas: 1142, conversion: 5.2 },
       { month: 'Abril', flujo: 18631, boletas: 1104, conversion: 5.9 },
       { month: 'Mayo', flujo: 18093, boletas: 1044, conversion: 5.8 },
-      { month: 'Junio', flujo: 11510, boletas: 667, conversion: 5.8 }
+      { month: 'Junio', flujo: 15034, boletas: 849, conversion: 5.6 },
+      { month: 'Julio', flujo: 3869, boletas: 186, conversion: 4.8 }
     ]
   },
   {
@@ -84,7 +91,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 19264, boletas: 889, conversion: 4.6 },
       { month: 'Abril', flujo: 15972, boletas: 911, conversion: 5.7 },
       { month: 'Mayo', flujo: 16708, boletas: 838, conversion: 5.0 },
-      { month: 'Junio', flujo: 12413, boletas: 713, conversion: 5.7 }
+      { month: 'Junio', flujo: 16689, boletas: 982, conversion: 5.9 },
+      { month: 'Julio', flujo: 3832, boletas: 216, conversion: 5.6 }
     ]
   },
   {
@@ -95,7 +103,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 9579, boletas: 420, conversion: 4.4 },
       { month: 'Abril', flujo: 6171, boletas: 242, conversion: 3.9 },
       { month: 'Mayo', flujo: 5495, boletas: 276, conversion: 5.0 },
-      { month: 'Junio', flujo: 4680, boletas: 228, conversion: 4.9 }
+      { month: 'Junio', flujo: 5047, boletas: 240, conversion: 4.8 },
+      { month: 'Julio', flujo: 1004, boletas: 40, conversion: 4.0 }
     ]
   },
   {
@@ -106,7 +115,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 6237, boletas: 526, conversion: 8.4 },
       { month: 'Abril', flujo: 5529, boletas: 566, conversion: 10.2 },
       { month: 'Mayo', flujo: 6970, boletas: 607, conversion: 8.7 },
-      { month: 'Junio', flujo: 7113, boletas: 659, conversion: 9.3 }
+      { month: 'Junio', flujo: 7598, boletas: 706, conversion: 9.3 },
+      { month: 'Julio', flujo: 641, boletas: 64, conversion: 10.0 }
     ]
   },
   {
@@ -117,7 +127,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 5561, boletas: 791, conversion: 14.2 },
       { month: 'Abril', flujo: 4031, boletas: 549, conversion: 13.6 },
       { month: 'Mayo', flujo: 5015, boletas: 806, conversion: 16.1 },
-      { month: 'Junio', flujo: 3989, boletas: 556, conversion: 13.9 }
+      { month: 'Junio', flujo: 4248, boletas: 610, conversion: 14.4 },
+      { month: 'Julio', flujo: 891, boletas: 131, conversion: 14.7 }
     ]
   },
   {
@@ -128,7 +139,8 @@ export const initialStoreData = [
       { month: 'Marzo', flujo: 93066, boletas: 7306, conversion: 7.85 },
       { month: 'Abril', flujo: 77069, boletas: 6626, conversion: 8.60 },
       { month: 'Mayo', flujo: 81140, boletas: 6797, conversion: 8.38 },
-      { month: 'Junio', flujo: 76253, boletas: 6613, conversion: 8.67 }
+      { month: 'Junio', flujo: 81278, boletas: 7074, conversion: 8.7 },
+      { month: 'Julio', flujo: 17457, boletas: 1370, conversion: 7.85 }
     ]
   }
 ];
